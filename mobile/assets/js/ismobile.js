d22a9ec3cecfda7494838161d02347219e54519d
yzz <2419349086@qq.com>
2020-04-21 16:46:54 +0800

    跳转

--- mobile/assets/js/ismobile.js
+++ mobile/assets/js/ismobile.js
@@ -4,14 +4,18 @@ $(function(){
     var path = window.location.pathname
     var params = window.location.search;
     var host ='http://www.hdlbdd.com';
-    if((/yushan33\.cn/).test(headerHost)) {
-        if((/hdl02\.yushan33\.cn/).test(headerHost)){
-            host = 'http://hdl01.yushan33.cn';
-        }
-	} else if(!(/hdlbdd\.com/).test(headerHost)) {
+    if(!(/hdlbdd\.com/).test(headerHost)) {
 		//测试环境
 		host = 'http://www.gdzb.bddia.com';
 	}
+    // if((/yushan33\.cn/).test(headerHost)) {
+    //     if((/hdl02\.yushan33\.cn/).test(headerHost)){
+    //         host = 'http://hdl01.yushan33.cn';
+    //     }
+	// } else if(!(/hdlbdd\.com/).test(headerHost)) {
+	// 	//测试环境
+	// 	host = 'http://www.gdzb.bddia.com';
+	// }
     console.log("mobile_flag",window.location)
     console.log("mobile",host + path + params)
     if(!mobile_flag){
