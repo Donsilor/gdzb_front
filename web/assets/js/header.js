90166bd6d6789b2711abce0f6712c304dfe64c45
donsilor <390807392@qq.com>
2021-01-27 17:06:23 +0800

    updata

--- web/assets/js/header.js
+++ web/assets/js/header.js
@@ -5,11 +5,17 @@ $(function(){
     $(".title_nav li").mouseleave(function() {
         $(this).find('.nav_box').hide();
     });
-    
-    
+
         
     // $("#div").click(function() {
     //     str = $("#div").text().substr(0,10) + " ...";
     //      $(".content p").text(str);
     // })
+
+
+    // 获取页面底部公共年份
+    var data = new Date(),
+        year = data.getFullYear();
+    
+    $('.year').text(year)
 })
